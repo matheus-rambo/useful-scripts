fd672d560ebd206eab19d9395f4b8f128f536ad6
Matheus Felipe Rambo Blume <mblume@grupodimed.com.br>
2021-02-03 11:18:36 -0300

    Ajustes no script

--- Linux/ascii-art/config.sh
+++ Linux/ascii-art/config.sh
@@ -5,19 +5,13 @@ FILES="*.asciiart";
 SCRIPT="ascii-selector.sh";
 SHELL_FILE=".bashrc";
 
-function install() {
-   mkdir ${DIR} || echo "Can not create the directory ${DIR} aborting..."; exit 1;
-   echo "Copying all files: asciiart to: ${DIR}";
-   cp ${FILES} ${SCRIPT} ${DIR}/ || echo "Can not copy files, aborting ..."; rm -R ${DIR}; exit 1;
-   echo "Adding new line to ${SHELL_FILE} ...";
-   echo -e "\n${DIR}/./${SCRIPT}" >> ~/${SHELL_FILE} || echo "Can not append a line to the ${SHELL_FILE}, abortin ..."; rm -R${DIR}; exit 1;
-  echo "Done";
-  return 0;
-}
+mkdir ${DIR} || exit 1;
+echo "Copying all files: asciiart to: ${DIR}";
+cp ${FILES} ascii-selector.sh ${DIR}/ || exit 2;
+echo "Adding new line to ${SHELL_FILE} ...";
+echo -e "\n${DIR}/./${SCRIPT}" >> ~/${SHELL_FILE} ||  exit 3;
+echo "Done";
 
-function remove() {
-   return 0;
-}
 
 
 
