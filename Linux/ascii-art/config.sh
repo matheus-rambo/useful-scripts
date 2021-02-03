9a7b27b1eee5e92dce705f31db2c5f9b34b5008f
Matheus Rambo <33197461+matheus-rambo@users.noreply.github.com>
2021-02-03 11:19:00 -0300

    Update config.sh

--- Linux/ascii-art/config.sh
+++ Linux/ascii-art/config.sh
@@ -9,7 +9,7 @@ mkdir ${DIR} || exit 1;
 echo "Copying all files: asciiart to: ${DIR}";
 cp ${FILES} ascii-selector.sh ${DIR}/ || exit 2;
 echo "Adding new line to ${SHELL_FILE} ...";
-echo -e "\n${DIR}/./${SCRIPT}" >> ~/${SHELL_FILE} ||  exit 3;
+echo -e "\n${DIR}/./${SCRIPT}\nsleep 0.5\nclear" >> ~/${SHELL_FILE} ||  exit 3;
 echo "Done";
 
 
