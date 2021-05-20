83598f5af56c21ccbb169ca8979ed97d05a1f5ca
Matheus Rambo <mrambo@grupodimed.com.br>
2021-05-20 16:27:04 -0300

    Update ascii art

--- Linux/bash/ascii-art/config.sh
+++ Linux/bash/ascii-art/config.sh
@@ -5,9 +5,9 @@ FILES="*.asciiart";
 SCRIPT="ascii-selector.sh";
 SHELL_FILE=".bashrc";
 
-mkdir ${DIR} || exit 1;
+mkdir -p ${DIR};
 echo "Copying all files: asciiart to: ${DIR}";
-cp ${FILES} ascii-selector.sh ${DIR}/ || exit 2;
+cp ${FILES} ascii-selector.sh ${DIR};
 echo "Adding new line to ${SHELL_FILE} ...";
 echo -e "\n${DIR}/./${SCRIPT}\nsleep 0.5\nclear" >> ~/${SHELL_FILE} ||  exit 3;
 echo "Done";
